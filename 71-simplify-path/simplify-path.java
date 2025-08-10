0577f1f9afd170650c960ca8a1c4c6459aec7a86
Chilakalapalli GayathriPrasanna <gayathri162004@gmail.com>
2025-08-10 20:42:49 +0800

    Time: 6 ms (28.12%) | Memory: 44 MB (25.34%) - LeetSync

---
+++ 71-simplify-path/simplify-path.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String simplifyPath(String path) {
+        Deque<String> stack = new ArrayDeque<>();
+        for (String segment : path.split("/")) {
+            if (segment.isEmpty() || ".".equals(segment)) {
+                continue;
+            }
+            if ("..".equals(segment)) {
+                if (!stack.isEmpty()) stack.pollLast();
+            } else {
+                stack.offerLast(segment);
+            }
+        }
+        return "/" + String.join("/", stack);
+    }
+}
